760b888584bc3a45a6af90675b4feb35d0c9e359
maynunes <31082181+maynunes@users.noreply.github.com>
2017-09-19 21:00:54 -0300

    PROJETO WEB

---
+++ AngularJS/js/zumbis.controller.js
@@ -0,0 +1,12 @@
+angular.module('app').controller('zumbisCtrl', function($scope, $rootScope) {
+    $rootScope.$on('cep', function(event, data) {
+        $scope.ocorrencia.bairro = data.bairro;
+        $scope.ocorrencia.cidade = data.cidade;
+    });
+
+    $scope.ocorrencias = [];
+
+    $scope.adiciona = function(ocorrencia) {
+        $scope.ocorrencias.push(angular.copy(ocorrencia));
+    };
+});
